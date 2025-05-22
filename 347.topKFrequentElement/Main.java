d629c3a3b154cc3a56bb698f7373e661fa3d1b84
utkarshkrsingh <utkarsh.isa.dev@gmail.com>
2025-05-22 09:45:00 +0530

    day07 --updated

---
+++ 347.topKFrequentElement/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1,2};
+        int k = 2;
+        topKFrequent(nums, k);
+    }
+
+    static int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        List<int[]> arr = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            arr.add(new int[] {entry.getValue(), entry.getKey()});
+        }
+        arr.sort((a, b) -> b[0] - a[0]);
+
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = arr.get(i)[1];
+        }
+        return res;
+    }
+}
